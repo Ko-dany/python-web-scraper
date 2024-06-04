8b2dd42be8dc5ebdc6a74b80cb92d1f57f779bb7
Dahyun (Dany) <88699158+Ko-dany@users.noreply.github.com>
2024-06-03 21:02:22 -0400

    Get HTML content to parse data using bs4

--- functions/communitech_scraper.py
+++ functions/communitech_scraper.py
@@ -1,25 +1,83 @@
+import asyncio
 from playwright.sync_api import sync_playwright
 import time
 from bs4 import BeautifulSoup
 import csv
 
-playwright = sync_playwright().start()
-browser = playwright.chromium.launch(headless=False)
-url_communitech = "https://www1.communitech.ca/jobs"
+def is_scroll_at_bottom(page):
+    scroll_height = page.evaluate('(document.documentElement || document.body).scrollHeight')
+    client_height = page.evaluate('(window.innerHeight || document.documentElement.clientHeight)')
+    scroll_top = page.evaluate('(document.documentElement || document.body).scrollTop')
 
-page = browser.new_page()
-page.goto(url_communitech)
+    return scroll_height - client_height <= scroll_top + 1
 
-time.sleep(2) # Waits for 2 seconds
-page.get_by_placeholder("Location").click()
-print("Click Location")
+def communictech_scraper():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        url_communitech = "https://www1.communitech.ca/jobs"
 
-time.sleep(2)
-page.get_by_placeholder("Type to search").fill("Waterloo Region")
-print("Fill Location")
+        page = browser.new_page()
+        page.goto(url_communitech)
 
-time.sleep(2)
-page.click(".sc-beqWaB.kQSjka:first-of-type") # Selects the first item - "Waterloo Region"
-print("Select option")
+        time.sleep(2) # Waits for 2 seconds
+        page.get_by_placeholder("Location").click()
 
-time.sleep(5)
+        time.sleep(2)
+        page.get_by_placeholder("Type to search").fill("Waterloo Region")
+
+        time.sleep(2)
+        page.click(".sc-beqWaB.kQSjka:first-of-type") # Selects the first item - "Waterloo Region"
+
+        time.sleep(2)
+        page.get_by_placeholder("Job title, company or keyword").fill("test")
+
+        # Load more if needed
+        '''
+        while True:
+            button = page.query_selector('[data-testid="load-more"]')
+            if button:
+                button.click()
+                time.sleep(5)                
+                page.keyboard.down("End")
+
+                while not is_scroll_at_bottom(page):
+                    loading = page.query_selector('[class="sc-beqWaB dCSNrJ"]')
+                    if loading:
+                        time.sleep(3)          
+                    page.keyboard.down("End")
+                    loading.scroll_into_view_if_needed()
+            else:
+                break
+        '''
+
+        time.sleep(2)
+        content = page.content()
+        job_list = []
+
+        browser.close()
+
+        soup = BeautifulSoup(content, "html.parser")
+        jobs = soup.find_all("div", class_="job-card")
+
+        for job in jobs:
+            company = job.find("div", itemprop="hiringOrganization").find("meta", itemprop="name")["content"]
+            title = job.find('div', itemprop='title').text.strip()
+            location_tags = job.find("div", itemprop="jobLocation").find_all("span")
+            locations = []
+            for tag in location_tags:
+                locations.append(tag.text)
+            url = f"https://www1.communitech.ca{job.find("div", class_="job-info").find("a")["href"]}"
+
+            job_list.append({
+                "title":title,
+                "company": company,
+                "location": locations,
+                "url":url
+            })
+
+            print(company, title, locations, url)
+            print("=============================")
+
+        print(f"Total {len(job_list)} jobs!")
+
+communictech_scraper()
