56357a24f9b8076a9a9598151cc3307e94c73e4f
Dahyun (Dany) <88699158+Ko-dany@users.noreply.github.com>
2024-06-03 23:52:12 -0400

    Connect Flask and React projects

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 db = {}
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/api/data")
+def get_data():
+    data = {
+        "message":"Hello from Flask!"
+    }
+    return jsonify(data)
 
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,27 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:5000/api/data")
+      .then((response) => {
+        setData(response.data.message);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <header>
+        <h1>Hello from React!</h1>
+        <h1>{data}</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
