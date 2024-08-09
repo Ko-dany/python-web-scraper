a85306e6c81f95bb94d2a5290c600e8fd79bff17
Dahyun (Dany) <88699158+Ko-dany@users.noreply.github.com>
2024-08-08 21:26:54 -0400

    Add progress bar when loading

--- frontend/src/components/Pages/Search/Search.tsx
+++ frontend/src/components/Pages/Search/Search.tsx
@@ -3,53 +3,65 @@ import { Outlet, Link } from "react-router-dom";
 import axios from "axios";
 
 import "./Search.css";
-import { Box, Button, Divider, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Divider,
+  LinearProgress,
+  TextField,
+  Typography,
+} from "@mui/material";
 import Result from "../Result/Result";
 
 function Search() {
-  const [data, setData] = useState(false);
-  const [jobs, setJobs] = useState(false);
+  // const [data, setData] = useState(false);
+  const [loadingJobs, setLoadingJobs] = useState(false);
+  const [jobs, setJobs] = useState(null);
 
   const [keyword, setKeyword] = useState("");
   const [response, setResponse] = useState("");
 
   // Inital Fetching
-  useEffect(() => {
-    axios
-      .get("http://127.0.0.1:5000/api/data")
-      .then((response) => {
-        setData(response.data.message);
-      })
-      .catch((error) => {
-        console.error("Error fetching data: ", error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("http://127.0.0.1:5000/api/data")
+  //     .then((response) => {
+  //       setData(response.data.message);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching data: ", error);
+  //     });
+  // }, []);
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
 
-    console.log(keyword);
+    setLoadingJobs(true);
+
+    // console.log(keyword);
     try {
-      console.log("Starting Playwright...");
+      // console.log("Starting Playwright...");
       const res: any = await axios.post("http://localhost:5000/search", {
         keyword: keyword,
       });
 
-      console.log("Finishing Playwright...");
-
+      // console.log("Finishing Playwright...");
       setJobs(res.data);
-      console.log(jobs);
+      // console.log(jobs);
 
       setResponse("");
       setKeyword("");
     } catch (error) {
       if (axios.isAxiosError(error) && error.response) {
-        console.log(error.response.data);
+        // console.log(error.response.data);
         setResponse(error.response.data);
       } else {
+        // console.log("Error occurred: ", error, response);
         setResponse("Error: Could not get a response from the server.");
-        console.log("Error occurred: ", error, response);
       }
+    } finally {
+      setLoadingJobs(false);
     }
   };
 
@@ -85,7 +97,8 @@ function Search() {
             SEARCH
           </Button>
         </Box>
-        {jobs && <Result jobs={jobs}></Result>}
+        {loadingJobs && <LinearProgress />}
+        {!loadingJobs && jobs && <Result jobs={jobs} />}
         {/*<Divider></Divider>
          <Box>
           <Button>
